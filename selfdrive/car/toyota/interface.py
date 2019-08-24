d405feb79e36bb4aa5733995808e3ba2a7fde892
Raul C <48895835+srvolt@users.noreply.github.com>
2019-08-24 13:24:30 -0700

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -87,7 +87,18 @@ class CarInterface(object):
       # ret.lateralTuning.lqr.dcGain = 0.002237852961363602
 
       ret.steerActuatorDelay = 0.5
-
+      
+    elif candidate == CAR.PRIUS_2020:
+      stop_and_go = True
+      ret.safetyParam = 66  # see conversion factor for STEER_TORQUE_EPS in dbc file
+      ret.wheelbase = 2.70
+      ret.steerRatio = 15.74   # unknown end-to-end spec
+      tire_stiffness_factor = 0.6371   # hand-tune
+      ret.mass = 3045. * CV.LB_TO_KG + STD_CARGO_KG
+      
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.1]]
+      ret.lateralTuning.pid.kf = 0.00007818594
+      
     elif candidate in [CAR.RAV4, CAR.RAV4H]:
       stop_and_go = True if (candidate in CAR.RAV4H) else False
       ret.safetyParam = 73
